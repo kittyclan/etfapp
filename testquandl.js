8833796e166e9a060daead08fee4e6bd031633f0
randall <aznpwnzor@gmail.com>
2014-12-27 14:40:55 +0000

    started some mongo interaction

--- testquandl.js
+++ testquandl.js
@@ -1,15 +1,23 @@
-Quandl = require('quandl');
+var Quandl = require('quandl');
+var MongoClient = require('mongodb').MongoClient
+var assert = require('assert');
 
 var quandl = new Quandl({
     auth_token: '8ynsN6pzeBEjygp2sZGk',
     api_version: 1,
 });
 
+quandl.metadata({source: 'WIKI', table: 'AAPL', format: 'JSON'}, function(err, res){
+    if (err) {throw err};
+    jsonRes = JSON.parse(res);
+    console.log(jsonRes);
+    console.log('meta')
+});
 
 quandl.dataset({ source: 'WIKI', table: 'AAPL', format: 'JSON' }, function(err, res) {
     if (err) {throw err;}
     jsonRes = JSON.parse(res);
-    console.log(jsonRes.data[10])
+    console.log(jsonRes.data[10]);
 })
 
 
